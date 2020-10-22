2f47f4c3a164da9f6fda525e65561155d14c211d
Sergij18Klym <derstig@ukr.net>
2020-10-22 21:51:42 +0200

    author setup lesson2

--- appka.js
+++ appka.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require("express");
+const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
 
@@ -12,12 +13,14 @@ const app = express();
 const jsonParser = express.json();
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const urlka = "mongodb+srv://logineins:passwordeins@chern0nodejs1.ejdff.mongodb.net/chernobyl?retryWrites=true&w=majority"; 
 mongoose.connect(process.env.DATABASE_URL /*urlka*/, { useNewUrlParser: true });
@@ -26,5 +29,6 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
