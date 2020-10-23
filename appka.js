18940053ab4071d256e697b3330d3a90d2adc041
Sergij18Klym <derstig@ukr.net>
2020-10-23 23:56:55 +0200

    book update not yet

--- appka.js
+++ appka.js
@@ -1,5 +1,4 @@
-﻿
-if (process.env.NODE_ENV !== 'production') {
+﻿if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
@@ -7,28 +6,31 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require('method-override');
 
-const Schema = mongoose.Schema;
+//const Schema = mongoose.Schema;
 const app = express();
-const jsonParser = express.json();
+//const jsonParser = express.json();
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
-const urlka = "mongodb+srv://logineins:passwordeins@chern0nodejs1.ejdff.mongodb.net/chernobyl?retryWrites=true&w=majority"; 
-mongoose.connect(process.env.DATABASE_URL /*urlka*/, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 
 app.listen(process.env.PORT || 3000);
